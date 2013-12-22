6e9597119ec4070940e3fbe37c3f98596d207e95
Naaman Newbold <naaman@heroku.com>
2013-12-21 16:14:03 -0800

    fmt

--- bputils.go
+++ bputils.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-  "os"
-  "path/filepath"
-  "io"
-  "strings"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const executableFlag = 0111
 
 func executable(filemode os.FileMode) bool {
-  e := filemode & executableFlag
-  return e != 0
+	e := filemode & executableFlag
+	return e != 0
 }
 
 func recursiveCopy(from string, to string) filepath.WalkFunc {
-  toPath := to + "/"
+	toPath := to + "/"
 
-  return func(path string, info os.FileInfo, err error) error {
-    src, err := os.Open(path)
+	return func(path string, info os.FileInfo, err error) error {
+		src, err := os.Open(path)
 
-    if err != nil {
-      return err
-    }
+		if err != nil {
+			return err
+		}
 
-    defer src.Close()
+		defer src.Close()
 
-    dstPath := strings.Replace(path, from, toPath, 1)
+		dstPath := strings.Replace(path, from, toPath, 1)
 
-    if info.IsDir() {
-      os.MkdirAll(dstPath, info.Mode())
-    } else {
-      dst, err := os.Create(dstPath)
-      if err != nil {
-        return err
-      }
-      defer dst.Close()
-      _, err = io.Copy(dst, src)
-      return err
-    }
+		if info.IsDir() {
+			os.MkdirAll(dstPath, info.Mode())
+		} else {
+			dst, err := os.Create(dstPath)
+			if err != nil {
+				return err
+			}
+			defer dst.Close()
+			_, err = io.Copy(dst, src)
+			return err
+		}
 
-    return nil
-  }
+		return nil
+	}
 }
 
 func scriptIsValid(script string) bool {
-  scriptStat, err := os.Stat(script)
-  if err != nil || !executable(scriptStat.Mode()) {
-    return false
-  }
-  return true
+	scriptStat, err := os.Stat(script)
+	if err != nil || !executable(scriptStat.Mode()) {
+		return false
+	}
+	return true
 }
--- buildpack.go
+++ buildpack.go
@@ -1,103 +1,103 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "errors"
-  "io/ioutil"
-  "path/filepath"
-  "os/exec"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 type Buildpack struct {
-  detect  string
-  compile string
-  release string
+	detect  string
+	compile string
+	release string
 }
 
 func NewBuildpack(basedir string) (Buildpack, error) {
-  absBasedir, absBasedirErr := filepath.Abs(basedir)
-  if absBasedirErr != nil {
-    return Buildpack{}, absBasedirErr
-  }
-  absBindir := absBasedir + "/bin"
-  bindir, bindirErr := os.Stat(absBindir)
-  if bindirErr != nil || !bindir.IsDir() {
-    return Buildpack{}, errors.New("Buildpack does not contain a `bin` directory")
-  }
-  newBuildpack := Buildpack {
-    detect:  absBindir + "/detect",
-    compile: absBindir + "/compile",
-    release: absBindir + "/release",
-  }
-  if !newBuildpack.checkScripts() {
-    return Buildpack{}, errors.New("Buildpack does not have executable detect, compile, and release scripts.")
-  }
-  return newBuildpack, nil
+	absBasedir, absBasedirErr := filepath.Abs(basedir)
+	if absBasedirErr != nil {
+		return Buildpack{}, absBasedirErr
+	}
+	absBindir := absBasedir + "/bin"
+	bindir, bindirErr := os.Stat(absBindir)
+	if bindirErr != nil || !bindir.IsDir() {
+		return Buildpack{}, errors.New("Buildpack does not contain a `bin` directory")
+	}
+	newBuildpack := Buildpack{
+		detect:  absBindir + "/detect",
+		compile: absBindir + "/compile",
+		release: absBindir + "/release",
+	}
+	if !newBuildpack.checkScripts() {
+		return Buildpack{}, errors.New("Buildpack does not have executable detect, compile, and release scripts.")
+	}
+	return newBuildpack, nil
 }
 
 func (b *Buildpack) checkScripts() bool {
-   return scriptIsValid(b.detect) &&
-          scriptIsValid(b.compile) &&
-          scriptIsValid(b.release)
+	return scriptIsValid(b.detect) &&
+		scriptIsValid(b.compile) &&
+		scriptIsValid(b.release)
 }
 
 func (b *Buildpack) Run(appdir string) (int, error) {
-  buildEnv := newBuildEnv(appdir)
+	buildEnv := newBuildEnv(appdir)
 
-  detectCmd := exec.Command(b.detect, buildEnv.buildDir)
-  detectOut, detectErr := detectCmd.CombinedOutput()
-  if detectErr != nil {
-    return -1, detectErr
-  }
-  compileCmd := exec.Command(b.compile, buildEnv.buildDir, buildEnv.cacheDir, buildEnv.envFile)
-  compileOut, compileErr := compileCmd.CombinedOutput()
-  if compileErr != nil {
-    return -1, compileErr
-  }
-  releaseCmd := exec.Command(b.release, buildEnv.buildDir)
-  releaseOut, releaseErr := releaseCmd.CombinedOutput()
-  if releaseErr != nil {
-    return -1, releaseErr
-  }
-  fmt.Println(string(detectOut))
-  fmt.Println(string(compileOut))
-  fmt.Println(string(releaseOut))
-  
-  return 0, nil
+	detectCmd := exec.Command(b.detect, buildEnv.buildDir)
+	detectOut, detectErr := detectCmd.CombinedOutput()
+	if detectErr != nil {
+		return -1, detectErr
+	}
+	compileCmd := exec.Command(b.compile, buildEnv.buildDir, buildEnv.cacheDir, buildEnv.envFile)
+	compileOut, compileErr := compileCmd.CombinedOutput()
+	if compileErr != nil {
+		return -1, compileErr
+	}
+	releaseCmd := exec.Command(b.release, buildEnv.buildDir)
+	releaseOut, releaseErr := releaseCmd.CombinedOutput()
+	if releaseErr != nil {
+		return -1, releaseErr
+	}
+	fmt.Println(string(detectOut))
+	fmt.Println(string(compileOut))
+	fmt.Println(string(releaseOut))
+
+	return 0, nil
 }
 
 type BuildEnv struct {
-  buildDir string
-  cacheDir string
-  envFile  string
+	buildDir string
+	cacheDir string
+	envFile  string
 }
 
 func newBuildEnv(appdir string) BuildEnv {
-  buildDir, _ := ioutil.TempDir(os.TempDir(), "build")
-  filepath.Walk(appdir, recursiveCopy(appdir, buildDir))
-  cacheDir, _ := ioutil.TempDir(os.TempDir(), "cache")
+	buildDir, _ := ioutil.TempDir(os.TempDir(), "build")
+	filepath.Walk(appdir, recursiveCopy(appdir, buildDir))
+	cacheDir, _ := ioutil.TempDir(os.TempDir(), "cache")
 
-  envFile, _ := ioutil.TempFile(os.TempDir(), ".env")
-  return BuildEnv { 
-    buildDir: buildDir, 
-    cacheDir: cacheDir, 
-    envFile:  envFile.Name(),
-  }
+	envFile, _ := ioutil.TempFile(os.TempDir(), ".env")
+	return BuildEnv{
+		buildDir: buildDir,
+		cacheDir: cacheDir,
+		envFile:  envFile.Name(),
+	}
 }
 
 func main() {
-  baseDir := os.Args[1]
-  appDir := os.Args[2]
-  baseBuildpack, err := NewBuildpack(baseDir)
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    exit, runErr := baseBuildpack.Run(appDir)
-    if runErr != nil {
-      fmt.Println(runErr)
-    } else {
-      fmt.Println(exit)
-    }
-  }
+	baseDir := os.Args[1]
+	appDir := os.Args[2]
+	baseBuildpack, err := NewBuildpack(baseDir)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		exit, runErr := baseBuildpack.Run(appDir)
+		if runErr != nil {
+			fmt.Println(runErr)
+		} else {
+			fmt.Println(exit)
+		}
+	}
 }
